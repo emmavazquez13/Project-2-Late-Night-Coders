704f8966a737a43d9dc9c330aabe3adc0748fd9f
GH0ST <adamjamessherrill@gmail.com>
2022-11-07 22:01:22 -0600

    created homeRoutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const profileRoutes = require('./profileRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/profile', profileRoutes);
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
